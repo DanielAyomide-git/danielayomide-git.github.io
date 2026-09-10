10e6c09a322e4b2342bd0185b7e68de0ca3b06bc
DanielAyomide-git <lanredaniel377@gmail.com>
2026-09-10 17:15:36 +0100

    Updated CV

--- src/App.tsx
+++ src/App.tsx
@@ -263,7 +263,7 @@ function App() {
                   <p className="mt-6 mb-4 max-w-2xl text-base leading-8 text-slate-600 sm:text-lg">
                     My research is at the intersection of mathematics,
                     artificial intelligence and its applications to scientific
-                    discovery, quantum AI, quantum circuit compilation and
+                    discoveries, quantum AI, quantum circuit compilation and
                     optimization for near-term and fault-tolerance devices,
                     Hamiltonian simulation, classical simulation of quantum and
                     chemical systems, mathematical methods for quantum
